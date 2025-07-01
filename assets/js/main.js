c1ef0db15d7140209560a5a2dca758e598f97ea9
smiurgh9 <simurgh9@pm.me>
2025-06-30 21:58:30 -0500

    removed the stamp border.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,35 @@
-document.addEventListener('DOMContentLoaded', function () {
+function setLightMode(state, store = true) {
   const light = document.getElementById('light');
+  light.checked = state;
+  if (store) localStorage.setItem('tashfeen.org:light-mode', light.checked);
+  return light.checked;
+}
+
+function isLightMode() {
+  const light = document.getElementById('light');
+  return light.checked;
+}
+
+function initialMode() {
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) return 'dark';
+  else return 'light';
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  setLightMode(initialMode() === 'light', (store = false));
+  window
+    .matchMedia('(prefers-color-scheme: light)')
+    .addEventListener('change', (event) => {
+      setLightMode(event.matches);
+      localStorage.removeItem('tashfeen.org:light-mode');
+    });
+
   const isLight = localStorage.getItem('tashfeen.org:light-mode');
+  if (isLight !== null)
+    // something was stored
+    setLightMode(isLight === 'true');
 
-  if (isLight !== null) light.checked = isLight === 'true';
-
+  const light = document.getElementById('light');
   light.addEventListener('change', function () {
     localStorage.setItem('tashfeen.org:light-mode', this.checked);
   });
@@ -11,8 +37,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.addEventListener('keydown', function (event) {
   if (event.key.toUpperCase() === 'L') {
-    const light = document.getElementById('light');
-    light.checked = !light.checked;
-    localStorage.setItem('tashfeen.org:light-mode', light.checked);
+    setLightMode(!isLightMode());
   }
 });

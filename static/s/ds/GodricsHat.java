1129479937a7539a8fd3bebe023b86e6b35129f8
smiurgh9 <simurgh9@pm.me>
2025-09-10 14:40:39 -0500

    added ds hw3

---
+++ static/s/ds/GodricsHat.java
@@ -0,0 +1,7 @@
+interface GodricsHat {
+  void insertion(int[] array);
+  void merge(int[] array);               // use recursion only
+  void quick(int[] array, int p, int r); // use recursion only
+  void quickLoopy(int[] array);
+  void counting(int[] array);
+}
